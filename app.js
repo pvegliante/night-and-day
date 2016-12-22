f639c6cfd63abd52512e99d238ecb4cc3b24cc6c
FVI <test@example.com>
2016-12-22 16:52:43 -0500

    add smiles

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 var button = document.getElementById('button');
 var body = document.getElementById('body');
 var orb = document.getElementById('orb');
+var smile = document.getElementById('smile');
+var skull = document.getElementById('skull');
 
 button.addEventListener('click', function() {
     if (body.classList.contains('night')) {
         body.classList.remove('night');
         body.classList.add('day');
+        skull.classList.add('hidden');
+        smile.classList.remove('hidden');
 
         orb.classList.remove('moon');
         orb.classList.add('sun');
@@ -15,6 +19,8 @@ button.addEventListener('click', function() {
         body.classList.remove('day');
         orb.classList.remove('sun');
         orb.classList.add('moon');
+        smile.classList.add('hidden');
+        skull.classList.remove('hidden');
 
     }
     //  body.classList.toggle('night');
